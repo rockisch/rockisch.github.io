a65ee30e5d1395e1205034d69bda642a1925de5d
rockisch <rockischplay@gmail.com>
2018-03-20 21:23:15 -0300

    refactor

--- js/fractralTree.js
+++ js/fractralTree.js
@@ -1,73 +1,73 @@
-var canvas = document.getElementById("myCanvas");
+'use strict';
+
+const canvas = document.getElementById("myCanvas");
 var width = canvas.offsetWidth;
 var heigth = canvas.offsetHeight;
 canvas.style.backgroundColor = "grey";
+const ctx = canvas.getContext("2d");
 
-var ctx = canvas.getContext("2d");
+const angle = Math.PI / 2;
 
 var len = 150;
-var angle = Math.PI / 2;
 var angleChange = Math.PI / 4;
-var newLenMult = 0.75
+var newLenMult = 0.75;
 var lenLimit = 10;
 var lineMaxWidth = 30;
 var lineWidthPerLayer = 3;
-var lineCounter = 0;
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// Actual drawing code. Will move to the place the last line was drawn, and then will drawn a line with the specified angle
+function branch(xMoveTo, yMoveTo, len, angle, lineCounter, angleBool) {
+	var newLineCounter = lineCounter + 1;
+	ctx.lineWidth = lineMaxWidth - (newLineCounter * lineWidthPerLayer);
+	var xLineTo = xMoveTo + (Math.cos(angle) * len);
+	var yLineTo = yMoveTo - (Math.sin(angle) * len);
+	ctx.lineTo(xLineTo, yLineTo);
+	ctx.stroke();
+	
+	len = len * newLenMult;
+	angle = angle + angleChange * angleBool;
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-	output.innerHTML = this.value / 4;
-	angleChange = Math.PI / (this.value / 4);
-	ctx.clearRect(0, 0, width, heigth)
-	branchCaller(width / 2, heigth, len, angle, 0)
+	if (len > lenLimit) {
+		branchCaller(xLineTo, yLineTo, len, angle, newLineCounter);
+	}
 }
 
-var slider2 = document.getElementById("len");
-var output2 = document.getElementById("demo2");
-output2.innerHTML = slider2.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider2.oninput = function() {
-	output2.innerHTML = this.value / 4;
-	len = this.value;
-	ctx.clearRect(0, 0, width, heigth)
-	branchCaller(width / 2, heigth, len, angle, 0)
-}
-
+// Will call the branch code 2 times with different mirrored angles to do the binary tree
 function branchCaller(xMoveTo, yMoveTo, len, angle, lineCounter) {
-	newLineCounter = lineCounter + 1;
-	ctx.lineWidth = lineMaxWidth - (newLineCounter * lineWidthPerLayer);
-	ctx.moveTo(xMoveTo, yMoveTo);
-	xLineTo = xMoveTo + (Math.cos(angle) * len);
-	yLineTo = yMoveTo - (Math.sin(angle) * len);
-	ctx.lineTo(xLineTo, yLineTo);
-	ctx.stroke();
-	
-	newLen = len * newLenMult;
-	newAngle = angle + angleChange;
-
-	if (len > lenLimit) {
-		branchCaller(xLineTo, yLineTo, newLen, newAngle, newLineCounter)
-	}
-	
-	newLineCounter = lineCounter + 1;
-	ctx.lineWidth = lineMaxWidth - (newLineCounter * lineWidthPerLayer);
+	branch(xMoveTo, yMoveTo, len, angle, lineCounter, 1);
+	branch(xMoveTo, yMoveTo, len, angle, lineCounter, -1);
 	ctx.beginPath(xMoveTo, yMoveTo);
-	xLineTo = xMoveTo + (Math.cos(angle) * len);
-	yLineTo = yMoveTo - (Math.sin(angle) * len);
-	ctx.lineTo(xLineTo, yLineTo);
-	ctx.stroke();
-
-	newLen = len * newLenMult;
-	newAngle = angle - angleChange;
-
-	if (len > lenLimit) {
-		branchCaller(xLineTo, yLineTo, newLen, newAngle, newLineCounter)
-	}
 }
 
-branchCaller(width / 2, heigth, len, angle, 0)
+// Moving to the start point and starting branching proccess
+// I created a separated function for this so that I don't need to call moveTo in every branch interaction
+function startBranching(xMoveTo, yMoveTo, len, angle, lineCounter) {
+	ctx.moveTo(width / 2, heigth);
+	branchCaller(xMoveTo, yMoveTo, len, angle, lineCounter)
+}
+
+startBranching(width / 2, heigth, len, angle, 0);
+
+
+// Sliders Code
+const angleChangeSlider = document.getElementById("angleChange");
+const output = document.getElementById("angleChangeOutput");
+output.innerHTML = "Angle: 45.0";
+
+angleChangeSlider.oninput = function() {
+	angleChange = Math.PI / (this.value / 4);
+	output.innerHTML = "Angle: " + (180 / (this.value / 4)).toFixed(1);
+	ctx.clearRect(0, 0, width, heigth);
+	startBranching(width / 2, heigth, len, angle, 0);
+}
+
+const lenSlider = document.getElementById("len");
+const lenSliderOutput = document.getElementById("lenOutput");
+lenSliderOutput.innerHTML = "Lenght: 150";
+
+lenSlider.oninput = function() {
+	len = this.value;
+	lenSliderOutput.innerHTML = "Lenght: " + len;
+	ctx.clearRect(0, 0, width, heigth);
+	startBranching(width / 2, heigth, len, angle, 0);
+}
