34c8eec6288f7815969f02bbd71ebf5054861eef
rockisch <rockischplay@gmail.com>
2018-03-24 01:34:45 -0300

    a

--- js/fractralTree.js
+++ js/fractralTree.js
@@ -1,19 +1,46 @@
-'use strict';
+'use strict'
 
 const canvas = document.getElementById("myCanvas");
-var width = canvas.offsetWidth;
-var heigth = canvas.offsetHeight;
 canvas.style.backgroundColor = "grey";
 const ctx = canvas.getContext("2d");
 
-const angle = Math.PI / 2;
+const canvasUl = document.getElementById("projectUl")
+const canvasUlStyle = window.getComputedStyle(canvasUl, null);
+var canvasUlPadding = canvasUlStyle.getPropertyValue('padding-left');
+canvasUlPadding = canvasUlPadding.slice(0, -2);
 
-var len = 150;
-var angleChange = Math.PI / 4;
-var newLenMult = 0.75;
-var lenLimit = 10;
-var lineMaxWidth = 30;
-var lineWidthPerLayer = 3;
+var width = canvas.offsetWidth;
+var height = canvas.offsetHeight;
+
+const angle = Math.PI / 2;
+var canvasSizeMult = width / 1160
+
+var len;
+var angleChange;
+var newLenMult;
+var lenLimit;
+var lineMaxWidth;
+var lineWidthPerLayer;
+
+function setup() {
+	len = 130 * canvasSizeMult;
+	angleChange = Math.PI / 4;
+	newLenMult = .77;
+	lenLimit = 10 / canvasSizeMult;
+	lineMaxWidth = 30 * canvasSizeMult;
+	lineWidthPerLayer = 3.12 * (canvasSizeMult);
+}
+
+setup();
+
+window.onresize = () => {
+	width = canvas.width=canvasUl.offsetWidth - canvasUlPadding * 2;
+	height = canvas.height= 970 * canvasSizeMult;
+	canvasSizeMult = width / 1160
+	setup();
+	ctx.clearRect(0, 0, width, height);
+	startBranching(width / 2, height, len, angle, 0);
+}
 
 // Actual drawing code. Will move to the place the last line was drawn, and then will drawn a line with the specified angle
 function branch(xMoveTo, yMoveTo, len, angle, lineCounter, angleBool) {
@@ -42,11 +69,11 @@ function branchCaller(xMoveTo, yMoveTo, len, angle, lineCounter) {
 // Moving to the start point and starting branching proccess
 // I created a separated function for this so that I don't need to call moveTo in every branch interaction
 function startBranching(xMoveTo, yMoveTo, len, angle, lineCounter) {
-	ctx.moveTo(width / 2, heigth);
+	ctx.moveTo(width / 2, height);
 	branchCaller(xMoveTo, yMoveTo, len, angle, lineCounter)
 }
 
-startBranching(width / 2, heigth, len, angle, 0);
+startBranching(width / 2, height, len, angle, 0);
 
 
 // Sliders Code
@@ -57,17 +84,17 @@ output.innerHTML = "Angle: 45.0";
 angleChangeSlider.oninput = function() {
 	angleChange = Math.PI / (this.value / 4);
 	output.innerHTML = "Angle: " + (180 / (this.value / 4)).toFixed(1);
-	ctx.clearRect(0, 0, width, heigth);
-	startBranching(width / 2, heigth, len, angle, 0);
+	ctx.clearRect(0, 0, width, height);
+	startBranching(width / 2, height, len, angle, 0);
 }
 
 const lenSlider = document.getElementById("len");
 const lenSliderOutput = document.getElementById("lenOutput");
-lenSliderOutput.innerHTML = "Lenght: 150";
+lenSliderOutput.innerHTML = "Lenght: 150.0";
 
 lenSlider.oninput = function() {
-	len = this.value;
-	lenSliderOutput.innerHTML = "Lenght: " + len;
-	ctx.clearRect(0, 0, width, heigth);
-	startBranching(width / 2, heigth, len, angle, 0);
+	len = this.value * canvasSizeMult;
+	lenSliderOutput.innerHTML = "Lenght: " + len.toFixed(1);
+	ctx.clearRect(0, 0, width, height);
+	startBranching(width / 2, height, len, angle, 0);
 }
